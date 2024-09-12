a855935765425b263227d63395e27e056f5da3d0
Ridma Sathsara <158838958+Ridma-Sathsara@users.noreply.github.com>
2024-09-13 01:10:59 +0530

    add bottom navigation bar

--- lib/screens/user_Screen.dart
+++ lib/screens/user_Screen.dart
@@ -1,14 +1,15 @@
 import 'package:agro_mart/screens/login_screen.dart';
 import 'package:agro_mart/services/auth_services.dart';
 import 'package:flutter/material.dart';
+import './buyer/cart_page.dart'; // Import the CartPage class
+import './buyer/profile_page.dart'; // Import the ProfilePage class
 
 class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User Page"),
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: false, // Remove back button
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -22,41 +23,249 @@ class UserScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: HomePage(), // Your HomePage content
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  int _currentIndex = 0; // Track the selected tab
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      setState(() {});
+    }
+  }
+
+  // Method to handle bottom navigation taps
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    // Navigate based on selected tab
+    switch (index) {
+      case 0:
+        // Home tab selected
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        break;
+      case 1:
+        // Search tab selected
+        // Handle search navigation if needed
+        break;
+      case 2:
+        // Cart tab selected
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => CartPage()),
+        );
+        break;
+      case 3:
+        // Community tab selected
+        // Handle community navigation if needed
+        break;
+      case 4:
+        // Profile tab selected
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilePage()),
+        );
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
-            child: Text("Welcome, User!"),
-          ),
-          SizedBox(
-              height: 20), // Adds some space between the text and the button
-          ElevatedButton(
-            onPressed: () {
-              // Add the functionality for the button here
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("Pressed"),
-                    content: Text("You pressed the button!"),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("OK"),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            child: Text("Press Me"), // Button label
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          _buildCustomTopBar(),
+          _buildCategorySection(),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome to Agriculture App',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await AuthServices().signOut();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: Text('Logout and Return to Login'),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Community'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: 'Profile'),
+        ],
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+
+  Widget _buildCustomTopBar() {
+    return Container(
+      color: Colors.green,
+      padding: EdgeInsets.symmetric(vertical: 60), // Adjusted padding
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Icon(Icons.menu, color: Colors.white),
+          ),
+          Text(
+            'Agriculture App',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Icon(Icons.notifications, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategorySection() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Categories',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          _buildCategoryCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategoryCard() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildCategoryItem('Vegetables', 'assets/vegetable.jpeg'),
+        _buildCategoryItem('Fruits', 'assets/fruits.jpeg'),
+        _buildCategoryItem('Seeds', 'assets/seeds.jpeg'),
+      ],
+    );
+  }
+
+  Widget _buildCategoryItem(String title, String imagePath) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CategoryPage(title: title),
+            ),
+          );
+        },
+        child: Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+                  image: DecorationImage(
+                    image: AssetImage(imagePath),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryPage extends StatelessWidget {
+  final String title;
+
+  CategoryPage({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text('Details for $title'),
       ),
     );
   }
