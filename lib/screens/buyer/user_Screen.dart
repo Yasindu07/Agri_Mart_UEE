5ac0554cf3e690cb604f70496564c53ec90b948f
Ridma Sathsara <158838958+Ridma-Sathsara@users.noreply.github.com>
2024-09-21 18:34:49 +0530

    remove app bar in user screen

--- lib/screens/buyer/user_Screen.dart
+++ lib/screens/buyer/user_Screen.dart
@@ -1,6 +1,7 @@
 import 'package:agro_mart/screens/buyer/buyer_bottomNav.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:agro_mart/services/auth_services.dart';
+// import 'package:agro_mart/services/auth_services.dart';
 import 'package:agro_mart/screens/login_screen.dart';
 // import 'buyer_bottomNav.dart'; // Import the BottomNav widget
 
@@ -8,6 +9,19 @@ class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   actions: [
+      //     IconButton(
+      //       icon: const Icon(Icons.logout),
+      //       onPressed: () async {
+      //         // Add logout functionality
+      //         await FirebaseAuth.instance.signOut();
+      //         Navigator.pushReplacement(context,
+      //             MaterialPageRoute(builder: (context) => const LoginScreen()));
+      //       },
+      //     )
+      //   ],
+      // ),
       body: BuyerBottomNav(),
       // bottomNavigationBar: BottomNav(), // Use BottomNav for navigation
     );
