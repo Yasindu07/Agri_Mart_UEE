be6b5c6797fb2501b7faaa5410b9fc52adafb226
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-10-07 20:12:58 +0530

    edit01

--- lib/screens/farmer/farmerScreen/farmer_home.dart
+++ lib/screens/farmer/farmerScreen/farmer_home.dart
@@ -13,10 +13,12 @@ class FarmerHomePage extends StatefulWidget {
 }
 
 class _FarmerHomePageState extends State<FarmerHomePage> {
-  final ProductService _productService = ProductService(); // Firestore Service
+  final ProductService _productService = ProductService();
   TextEditingController _searchController = TextEditingController();
   String searchQuery = '';
   Timer? _debounce;
+  String selectedCategory = '';
+  List<Product> products = []; // Define products list
 
   @override
   void initState() {
@@ -41,6 +43,25 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
     });
   }
 
+  void _selectCategory(String category) {
+    setState(() {
+      if (category == 'All') {
+        selectedCategory = '';
+      } else {
+        selectedCategory = category;
+      }
+      searchQuery = '';
+    });
+  }
+
+  int _getCategoryCount(String category) {
+    if (category == 'All') {
+      return products.length;
+    } else {
+      return products.where((product) => product.category == category).length;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -53,13 +74,12 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
           style: GoogleFonts.poppins(
             color: Colors.black,
             fontWeight: FontWeight.bold,
-            fontSize: 22.0,
+            fontSize: 20.0,
           ),
         ),
       ),
       body: Column(
         children: [
-          // Header with Product List title and Add button
           Row(
             children: [
               Spacer(),
@@ -71,7 +91,7 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
                   style: GoogleFonts.poppins(
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
-                    fontSize: screenWidth * 0.06,
+                    fontSize: 30,
                   ),
                 ),
               ),
@@ -107,15 +127,44 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
             ),
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  _buildCategoryCard(
-                      screenWidth, screenHeight, 'Vegetables', '5'),
-                  _buildCategoryCard(screenWidth, screenHeight, 'Fruits', '5'),
-                  _buildCategoryCard(screenWidth, screenHeight, 'Seeds', '5'),
-                  _buildCategoryCard(
-                      screenWidth, screenHeight, 'Dry Foods', '5'),
-                ],
+              child: StreamBuilder<List<Product>>(
+                stream: _productService.getUserProducts(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error loading products'));
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('No products found'));
+                  }
+
+                  products = snapshot.data!; // Store fetched products
+
+                  // Calculate counts dynamically
+                  int vegetableCount = _getCategoryCount('Vegetable');
+                  int fruitCount = _getCategoryCount('Fruits');
+                  int seedsCount = _getCategoryCount('Seeds');
+                  int dryFoodsCount = _getCategoryCount('Dry Foods');
+
+                  return Row(
+                    children: [
+                      _buildCategoryCard(screenWidth, screenHeight, 'All',
+                          _getCategoryCount('All')),
+                      _buildCategoryCard(screenWidth, screenHeight, 'Vegetable',
+                          vegetableCount),
+                      _buildCategoryCard(
+                          screenWidth, screenHeight, 'Fruits', fruitCount),
+                      _buildCategoryCard(
+                          screenWidth, screenHeight, 'Seeds', seedsCount),
+                      _buildCategoryCard(screenWidth, screenHeight, 'Dry Foods',
+                          dryFoodsCount),
+                    ],
+                  );
+                },
               ),
             ),
           ),
@@ -164,38 +213,53 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
 
   // Category Card Builder
   Widget _buildCategoryCard(
-      double screenWidth, double screenHeight, String title, String count) {
+      double screenWidth, double screenHeight, String title, int count) {
     double cardWidth = screenWidth * 0.3;
     double cardHeight = screenHeight * 0.1;
 
-    return SizedBox(
-      width: cardWidth,
-      height: cardHeight, // Ensures square cards
-      child: Card(
-        color: Theme.of(context).colorScheme.secondary,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Padding(
-          padding: EdgeInsets.all(screenWidth * 0.02), // Responsive padding
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.04, // Responsive font size
-                  fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () => _selectCategory(title),
+      child: SizedBox(
+        width: cardWidth,
+        height: cardHeight,
+        child: Card(
+          color: selectedCategory == title ||
+                  (selectedCategory.isEmpty && title == 'All')
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.secondary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: EdgeInsets.all(screenWidth * 0.02),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.04,
+                    fontWeight: FontWeight.bold,
+                    color: selectedCategory == title ||
+                            (selectedCategory.isEmpty && title == 'All')
+                        ? Theme.of(context).colorScheme.surface
+                        : Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: screenWidth * 0.01),
-              Text(
-                count,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.05,
-                  fontWeight: FontWeight.bold,
+                SizedBox(height: screenWidth * 0.01),
+                Text(
+                  count.toString(), // Display the count
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.05,
+                    fontWeight: FontWeight.bold,
+                    color: selectedCategory == title ||
+                            (selectedCategory.isEmpty && title == 'All')
+                        ? Theme.of(context).colorScheme.surface
+                        : Colors.black,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -205,37 +269,43 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
   // Product List Builder
   Widget _buildProductList() {
     return StreamBuilder<List<Product>>(
-      stream: _productService.getUserProducts(), // Stream from Firestore
+      stream: _productService.getUserProducts(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator()); // Loading state
+          return Center(child: CircularProgressIndicator());
         }
 
         if (snapshot.hasError) {
-          return Center(child: Text('Error loading products')); // Error state
+          return Center(child: Text('Error loading products'));
         }
 
         if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return Center(child: Text('No products found')); // No data state
+          return Center(child: Text('No products found'));
         }
 
-        final products = snapshot.data!
-            .where((product) =>
-                product.title.toLowerCase().contains(searchQuery) ||
-                product.category.toLowerCase().contains(searchQuery))
-            .toList();
+        products = snapshot.data!; // Store fetched products
 
-        if (products.isEmpty) {
+        final filteredProducts = products.where((product) {
+          final matchesSearch =
+              product.title.toLowerCase().contains(searchQuery) ||
+                  product.category.toLowerCase().contains(searchQuery);
+          final matchesCategory = selectedCategory.isEmpty ||
+              product.category.toLowerCase() == selectedCategory.toLowerCase();
+
+          return matchesSearch && matchesCategory;
+        }).toList();
+
+        if (filteredProducts.isEmpty) {
           return Center(child: Text('No matching products found'));
         }
 
         return ListView.builder(
           padding: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * 0.03),
-          itemCount: products.length,
+          itemCount: filteredProducts.length,
           itemBuilder: (context, index) {
             return _buildProductCard(
-                products[index],
+                filteredProducts[index],
                 MediaQuery.of(context).size.width,
                 MediaQuery.of(context).size.height);
           },
@@ -261,9 +331,9 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
               child: Image.network(
                 product.imageUrls.isNotEmpty
                     ? product.imageUrls[0]
-                    : 'https://via.placeholder.com/70', // Fallback image if no URL is available
+                    : 'https://via.placeholder.com/70',
                 width: screenWidth * 0.22,
-                height: screenWidth * 0.22, // Responsive image size
+                height: screenWidth * 0.22,
                 fit: BoxFit.cover,
               ),
             ),
@@ -297,12 +367,22 @@ class _FarmerHomePageState extends State<FarmerHomePage> {
                     ),
                   ),
                   SizedBox(height: screenHeight * 0.01),
-                  Text(
-                    'Quantity: ${product.quantity}',
-                    style: GoogleFonts.poppins(
-                      fontSize: screenWidth * 0.028,
-                      color: const Color.fromARGB(255, 99, 98, 98),
-                    ),
+                  Row(
+                    children: [
+                      Text(
+                        'Quantity: ${product.quantity}',
+                        style: GoogleFonts.poppins(
+                          fontSize: screenWidth * 0.028,
+                          color: const Color.fromARGB(255, 99, 98, 98),
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '${product.pricePerKg} RS',
+                        style: GoogleFonts.poppins(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
                 ],
               ),
